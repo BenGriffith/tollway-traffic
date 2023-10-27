47cdfa6d7c71d19fabcca7939462ce53fe0f6237
Ben Griffith <bengriffith@outlook.com>
2023-10-27 13:15:54 +0000

    started working on scraper to get US tollway names

--- tollway/vehicle.py
+++ tollway/vehicle.py
@@ -1,8 +1,13 @@
 from random import choice
 
+import requests
+from bs4 import BeautifulSoup
 from faker import Faker
 from faker_vehicle import VehicleProvider
 from us.states import STATES
+
+
+TOLLWAYS_URL = "https://en.wikipedia.org/wiki/List_of_toll_roads_in_the_United_States"
 
 
 def create_vehicle(fake: Faker) -> dict:
@@ -16,6 +21,13 @@ def create_vehicle(fake: Faker) -> dict:
     return vehicle
 
 
+def _tollways() -> list[str]:
+    tollways_html = requests.get(TOLLWAYS_URL)
+    soup = BeautifulSoup(markup=tollways_html.text, features="html.parser")
+
+    content = soup.find_all("div", "mw-parser-output")
+
+
 def create_tollway(tollways: list[str]) -> dict:
     pass
     # vehicle["tollway"]
@@ -27,6 +39,7 @@ def create_message() -> None:
     
 
 if __name__ == "__main__":
-    fake = Faker()
-    fake.add_provider(VehicleProvider)
-    vehicle = create_vehicle(fake=fake)
+    # fake = Faker()
+    # fake.add_provider(VehicleProvider)
+    # vehicle = create_vehicle(fake=fake)
+    _tollways()
