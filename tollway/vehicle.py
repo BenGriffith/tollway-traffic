78b32e2253475970b8c050cd2ca99aa85f65e8ab
Ben Griffith <bengriffith@outlook.com>
2023-10-27 12:46:36 +0000

    added create_tollway and create_message functions

--- tollway/vehicle.py
+++ tollway/vehicle.py
@@ -1,19 +1,32 @@
+from random import choice
+
 from faker import Faker
 from faker_vehicle import VehicleProvider
+from us.states import STATES
 
 
 def create_vehicle(fake: Faker) -> dict:
-
+    
     vehicle = {f"{key.lower()}": value for key, value in fake.vehicle_object().items()}
-    vehicle["license plate"] = fake.license_plate()
+    vehicle["license_plate"] = fake.license_plate()
     vehicle["vin"] = fake.vin()
-    # vehicle["state"] = 
+    vehicle["state"] = choice(STATES).abbr
+    vehicle["primary_color"] = fake.color_name()
 
     return vehicle
+
+
+def create_tollway(tollways: list[str]) -> dict:
+    pass
+    # vehicle["tollway"]
+    # vehicle[""]
+
+
+def create_message() -> None:
+    pass
     
 
 if __name__ == "__main__":
     fake = Faker()
     fake.add_provider(VehicleProvider)
     vehicle = create_vehicle(fake=fake)
-    breakpoint()
