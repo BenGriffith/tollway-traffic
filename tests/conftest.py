6d705f93b554772e1b2a8b5cbe329b8474a8b199
Ben Griffith <bengriffith@outlook.com>
2023-11-15 13:54:02 +0000

    added test_create_vehicle

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import pytest
 from faker import Faker
 from faker_vehicle import VehicleProvider
@@ -6,8 +8,48 @@ from tollway.vehicle import (
     create_vehicle, 
     get_tollways,
     create_tollway,
+    create_payload,
     STATES_AND_TERRITORIES,
 )
+
+
+@pytest.fixture
+def vehicle_constants():
+    vehicle_constants = {
+        "STATES": 29,
+        "ALABAMA": 4,
+        "CALIFORNIA": 16,
+        "COLORADO": 8,
+        "DELAWARE": 3,
+        "FLORIDA": 35,
+        "GEORGIA": 4,
+        "ILLINOIS": 6,
+        "INDIANA": 1,
+        "KANSAS": 1,
+        "LOUISIANA": 1,
+        "MAINE": 1,
+        "MARYLAND": 3,
+        "MASSACHUSETTS": 1,
+        "MINNESOTA": 4,
+        "NEW HAMPSHIRE": 3,
+        "NEW JERSEY": 5,
+        "NEW YORK": 3,
+        "NORTH CAROLINA": 3,
+        "OHIO": 1,
+        "OKLAHOMA": 11,
+        "PENNSYLVANIA": 6,
+        "PUERTO RICO": 8,
+        "RHODE ISLAND": 5,
+        "SOUTH CAROLINA": 1,
+        "TEXAS": 38,
+        "UTAH": 2,
+        "VIRGINIA": 12,
+        "WASHINGTON": 2,
+        "WEST VIRGINIA": 1,
+        "PAYLOAD_LENGTH": 11 
+    }
+
+    return vehicle_constants
 
 
 @pytest.fixture
@@ -32,4 +74,9 @@ def tollways():
 
 @pytest.fixture
 def get_tollway(tollways):
-    return create_tollway(tollways)
+    return create_tollway(tollways=tollways)
+
+
+@pytest.fixture
+def payload(vehicle, get_tollway):
+    return create_payload(vehicle=vehicle, tollway=get_tollway)
--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -6,40 +6,44 @@ def test_create_vehicle(setup, vehicle):
     assert vehicle.get("state") in [state.abbr for state in setup.get("states")]
 
 
-def test_get_tollways(tollways):
+def test_get_tollways(tollways, vehicle_constants):
     states = [state for state in tollways.keys()]
-    assert len(states) == 29
-    assert len(tollways.get("Alabama")) == 4
-    assert len(tollways.get("California")) == 16
-    assert len(tollways.get("Colorado")) == 8
-    assert len(tollways.get("Delaware")) == 3
-    assert len(tollways.get("Florida")) == 35
-    assert len(tollways.get("Georgia")) == 4
-    assert len(tollways.get("Illinois")) == 6
-    assert len(tollways.get("Indiana")) == 1
-    assert len(tollways.get("Kansas")) == 1
-    assert len(tollways.get("Louisiana")) == 1
-    assert len(tollways.get("Maine")) == 1
-    assert len(tollways.get("Maryland")) == 3
-    assert len(tollways.get("Massachusetts")) == 1
-    assert len(tollways.get("Minnesota")) == 4
-    assert len(tollways.get("New Hampshire")) == 3
-    assert len(tollways.get("New Jersey")) == 5
-    assert len(tollways.get("New York")) == 3
-    assert len(tollways.get("North Carolina")) == 3
-    assert len(tollways.get("Ohio")) == 1
-    assert len(tollways.get("Oklahoma")) == 11
-    assert len(tollways.get("Pennsylvania")) == 6
-    assert len(tollways.get("Puerto Rico")) == 8
-    assert len(tollways.get("Rhode Island")) == 5
-    assert len(tollways.get("South Carolina")) == 1
-    assert len(tollways.get("Texas")) == 38
-    assert len(tollways.get("Utah")) == 2
-    assert len(tollways.get("Virginia")) == 12
-    assert len(tollways.get("Washington")) == 2
-    assert len(tollways.get("West Virginia")) == 1
+    assert len(states) == vehicle_constants.get("STATES")
+    assert len(tollways.get("Alabama")) == vehicle_constants.get("ALABAMA")
+    assert len(tollways.get("California")) == vehicle_constants.get("CALIFORNIA")
+    assert len(tollways.get("Colorado")) == vehicle_constants.get("COLORADO")
+    assert len(tollways.get("Delaware")) == vehicle_constants.get("DELAWARE")
+    assert len(tollways.get("Florida")) == vehicle_constants.get("FLORIDA")
+    assert len(tollways.get("Georgia")) == vehicle_constants.get("GEORGIA")
+    assert len(tollways.get("Illinois")) == vehicle_constants.get("ILLINOIS")
+    assert len(tollways.get("Indiana")) == vehicle_constants.get("INDIANA")
+    assert len(tollways.get("Kansas")) == vehicle_constants.get("KANSAS")
+    assert len(tollways.get("Louisiana")) == vehicle_constants.get("LOUISIANA")
+    assert len(tollways.get("Maine")) == vehicle_constants.get("MAINE")
+    assert len(tollways.get("Maryland")) == vehicle_constants.get("MARYLAND")
+    assert len(tollways.get("Massachusetts")) == vehicle_constants.get("MASSACHUSETTS")
+    assert len(tollways.get("Minnesota")) == vehicle_constants.get("MINNESOTA")
+    assert len(tollways.get("New Hampshire")) == vehicle_constants.get("NEW HAMPSHIRE")
+    assert len(tollways.get("New Jersey")) == vehicle_constants.get("NEW JERSEY")
+    assert len(tollways.get("New York")) == vehicle_constants.get("NEW YORK")
+    assert len(tollways.get("North Carolina")) == vehicle_constants.get("NORTH CAROLINA")
+    assert len(tollways.get("Ohio")) == vehicle_constants.get("OHIO")
+    assert len(tollways.get("Oklahoma")) == vehicle_constants.get("OKLAHOMA")
+    assert len(tollways.get("Pennsylvania")) == vehicle_constants.get("PENNSYLVANIA")
+    assert len(tollways.get("Puerto Rico")) == vehicle_constants.get("PUERTO RICO")
+    assert len(tollways.get("Rhode Island")) == vehicle_constants.get("RHODE ISLAND")
+    assert len(tollways.get("South Carolina")) == vehicle_constants.get("SOUTH CAROLINA")
+    assert len(tollways.get("Texas")) == vehicle_constants.get("TEXAS")
+    assert len(tollways.get("Utah")) == vehicle_constants.get("UTAH")
+    assert len(tollways.get("Virginia")) == vehicle_constants.get("VIRGINIA")
+    assert len(tollways.get("Washington")) == vehicle_constants.get("WASHINGTON")
+    assert len(tollways.get("West Virginia")) == vehicle_constants.get("WEST VIRGINIA")
 
 
 def test_create_tollway(tollways, get_tollway):
     state, name = get_tollway
     assert name in tollways[state]
+
+
+def test_create_payload(payload, vehicle_constants):
+    assert len(payload) == vehicle_constants.get("PAYLOAD_LENGTH")
