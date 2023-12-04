23d4d323eb5a7eb139d7f245232cbf64070a9999
Ben Griffith <bengriffith@outlook.com>
2023-12-04 13:34:45 +0000

    added remaining typer tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,15 @@ runner = CliRunner()
         (["--total-events", -10], 2),  # callback
         (["--event-rate", -1], 2),  # callback
         (["--output-file", "True"], 2),  # invalid type
+        (["--output-filename", "invalid-format.csv"], 2),  # invalid format
+        (["--output-filename", "invalid-format.jsn"], 2),  # invalid format
+        (["--date-variation", "--include-late", "--include-duplicate"], 2),  # invalid combination
+        (["--date-variation", "--include-late"], 2),  # invalid combination
+        (["--date-variation", "--include-duplicate"], 2),  # invalid combination
+        (["--date-variation"], 0),
+        (["--include-late"], 0),
+        (["--include-duplicate"], 0),
+        (["--include-late", "--include-duplicate"], 0),  # valid combination
     ],
 )
 def test_app_inputs(test_input, expected):
