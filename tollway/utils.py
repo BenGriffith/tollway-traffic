eeb92ebe676d60109359798b0304298643b034c3
Ben Griffith <bengriffith@outlook.com>
2024-01-22 17:46:08 -0600

    added TollwayMessage

--- tollway/utils.py
+++ tollway/utils.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import TypedDict
+from typing import Optional, TypedDict
 
 from google.cloud import pubsub_v1
 from google.pubsub_v1 import PubsubMessage
@@ -8,23 +8,37 @@ from google.pubsub_v1 import PubsubMessage
 from tollway.constants import PROJECT_ID, TOPIC_ID
 
 
+class TollwayMessage(TypedDict):
+    year: str
+    make: str
+    model: str
+    category: str
+    license_plate: str
+    vin: str
+    state: str
+    primary_color: str
+    tollway_state: str
+    tollway_name: str
+    timestamp: str
+    is_late: Optional[str]
+    is_duplicate: Optional[bool]
+
+
 class EventsLog(TypedDict):
     late_events: dict[str, list[str]]
-    past_events: list[dict[str, str]]
-    all_events: list[dict[str, str]]
+    past_events: list[TollwayMessage]
+    all_events: list[TollwayMessage]
 
 
 def get_topic(pubsub: bool) -> tuple:
     if pubsub:
-        project_id = PROJECT_ID
-        topic_id = TOPIC_ID
         publisher = pubsub_v1.PublisherClient()
-        topic_path = publisher.topic_path(project=project_id, topic=topic_id)
+        topic_path = publisher.topic_path(project=PROJECT_ID, topic=TOPIC_ID)
         return publisher, topic_path
     return None, None
 
 
-def encode_message(message: dict) -> PubsubMessage:
+def encode_message(message: TollwayMessage) -> PubsubMessage:
     return PubsubMessage(json.dumps(message).encode("utf-8"))
 
 
