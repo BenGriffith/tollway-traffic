0460a1e051d8f6f9d14bc50479c657c1fe9a8804
Ben Griffith <bengriffith@outlook.com>
2024-01-26 22:01:15 -0600

    updated test cases for late and duplicate events

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
 import time
+from datetime import datetime
 
 import pytest
 from faker import Faker
 from faker_vehicle import VehicleProvider
 
+from tollway.constants import TIME_UNIT, TIMESTAMP_FORMAT
+from tollway.events import DuplicateEventProcessor, LateEventProcessor
+from tollway.utils import get_topic
 from tollway.vehicle import (
     STATES_AND_TERRITORIES,
     create_message,
@@ -14,15 +18,15 @@ from tollway.vehicle import (
 
 
 @pytest.fixture
-def setup():
+def faker_vehicle():
     fake = Faker()
     fake.add_provider(VehicleProvider)
     return {"faker": fake, "states": STATES_AND_TERRITORIES}
 
 
 @pytest.fixture
-def vehicle(setup):
-    return create_vehicle(fake=setup.get("faker"))
+def vehicle(faker_vehicle):
+    return create_vehicle(fake=faker_vehicle.get("faker"))
 
 
 @pytest.fixture(scope="session")
@@ -41,10 +45,10 @@ def message(vehicle, get_tollway):
 
 
 @pytest.fixture
-def messages(setup, get_tollway):
+def messages(faker_vehicle, get_tollway):
     _messages = []
     for _ in range(30):
-        vehicle = create_vehicle(fake=setup.get("faker"))
+        vehicle = create_vehicle(fake=faker_vehicle.get("faker"))
         message = create_message(vehicle=vehicle, tollway=get_tollway)
         _messages.append(message)
         time.sleep(0.1)
@@ -52,7 +56,7 @@ def messages(setup, get_tollway):
 
 
 @pytest.fixture
-def events_log():
+def events_log_init():
     events_log = {
         "late_events": {
             "seconds": [],
@@ -67,16 +71,85 @@ def events_log():
 
 
 @pytest.fixture
-def late_events(messages, events_log):
+def events_log(messages, events_log_init):
     late_events_messages = [message.get("timestamp") for message in messages]
-    events_log["late_events"]["seconds"] = late_events_messages
-    events_log["late_events"]["minutes"] = late_events_messages
-    events_log["late_events"]["hours"] = late_events_messages
-    events_log["late_events"]["days"] = late_events_messages
-    return events_log
+    events_log_init["late_events"]["seconds"] = late_events_messages
+    events_log_init["late_events"]["minutes"] = late_events_messages
+    events_log_init["late_events"]["hours"] = late_events_messages
+    events_log_init["late_events"]["days"] = late_events_messages
+    return events_log_init
 
 
 @pytest.fixture
-def past_events(messages, events_log):
-    events_log["past_events"] = messages
-    return events_log
+def past_events(messages, events_log_init):
+    events_log_init["past_events"] = messages
+    return events_log_init
+
+
+@pytest.fixture
+def pubsub():
+    return get_topic(pubsub=False)
+
+
+@pytest.fixture
+def late_events(faker_vehicle, events_log, pubsub, tollways):
+    late_event = LateEventProcessor(
+        events_log=events_log,
+        publisher=pubsub[0],
+        topic_path=pubsub[1],
+        fake=faker_vehicle["faker"],
+        tollways=tollways,
+    )
+    late_events = {}
+    for time_unit in TIME_UNIT.keys():
+        late_event.time_unit = time_unit
+        late_events[time_unit] = late_event.create_event()
+    return late_events
+
+
+@pytest.fixture
+def get_microseconds():
+    def _get_microseconds(ts: str):
+        return datetime.strptime(ts, TIMESTAMP_FORMAT).microsecond
+
+    return _get_microseconds
+
+
+@pytest.fixture
+def generate_event_pair(get_microseconds, late_events, events_log):
+    def _generate_event_pair(time_unit):
+        late_event_timestamp = late_events[time_unit]["timestamp"]
+        late_event_microseconds = get_microseconds(late_event_timestamp)
+
+        for original_event_timestamp in events_log["late_events"][time_unit]:
+            original_event_microseconds = get_microseconds(original_event_timestamp)
+            # Compare the microseconds part of the timestamps
+            if late_event_microseconds == original_event_microseconds:
+                return {
+                    "original": datetime.strptime(original_event_timestamp, TIMESTAMP_FORMAT),
+                    "late": datetime.strptime(late_event_timestamp, TIMESTAMP_FORMAT),
+                }
+        return None
+
+    return _generate_event_pair
+
+
+@pytest.fixture
+def original_and_late_events_timestamps(generate_event_pair):
+    event_pairs = {}
+    for time_unit in TIME_UNIT.keys():
+        event_pair = generate_event_pair(time_unit)
+        if event_pair:
+            event_pairs[time_unit] = event_pair
+    return event_pairs
+
+
+@pytest.fixture
+def duplicate_event(past_events, pubsub):
+    event = DuplicateEventProcessor(
+        events_log=past_events,
+        publisher=pubsub[0],
+        topic_path=pubsub[1],
+    )
+    duplicate = event.create_event()
+    return duplicate
--- tests/test_events.py
+++ tests/test_events.py
@@ -1,61 +1,18 @@
-from datetime import datetime
-
-from tollway.constants import TIME_UNIT, TIMESTAMP_FORMAT
-from tollway.events import create_duplicate_event, create_late_event
+from tollway.constants import TIME_UNIT
 
 
-def test_create_late_event_seconds(late_events, setup, tollways):
-    late_event_ts = create_late_event(
-        events_log=late_events, fake=setup.get("faker"), tollways=tollways, time_unit="seconds"
-    )
+def test_create_late_events(original_and_late_events_timestamps):
+    events = original_and_late_events_timestamps
+    seconds_difference = abs(events["seconds"]["original"] - events["seconds"]["late"]).seconds
+    minutes_difference = abs(events["minutes"]["original"] - events["minutes"]["late"]).seconds // 60
+    hours_difference = abs(events["hours"]["original"] - events["hours"]["late"]).seconds // 3600
+    days_difference = abs(events["days"]["original"] - events["days"]["late"]).days
 
-    late_event_ts = datetime.strptime(late_event_ts["timestamp"], TIMESTAMP_FORMAT)
-    for ts in late_events["late_events"]["seconds"]:
-        current_event_ts = datetime.strptime(ts, TIMESTAMP_FORMAT)
-        if late_event_ts.microsecond == current_event_ts.microsecond:
-            difference_in_seconds = abs(current_event_ts - late_event_ts)
-            assert TIME_UNIT["seconds"]["min"] <= difference_in_seconds.seconds <= TIME_UNIT["seconds"]["max"]
-
-
-def test_create_late_event_minutes(late_events, setup, tollways):
-    late_event_ts = create_late_event(
-        events_log=late_events, fake=setup.get("faker"), tollways=tollways, time_unit="minutes"
-    )
-
-    late_event_ts = datetime.strptime(late_event_ts["timestamp"], TIMESTAMP_FORMAT)
-    for ts in late_events["late_events"]["minutes"]:
-        current_event_ts = datetime.strptime(ts, TIMESTAMP_FORMAT)
-        if late_event_ts.microsecond == current_event_ts.microsecond:
-            difference_in_minutes = abs(current_event_ts - late_event_ts).seconds // 60
-            assert TIME_UNIT["minutes"]["min"] <= difference_in_minutes <= TIME_UNIT["minutes"]["max"]
+    assert TIME_UNIT["seconds"]["min"] <= seconds_difference <= TIME_UNIT["seconds"]["max"]
+    assert TIME_UNIT["minutes"]["min"] <= minutes_difference <= TIME_UNIT["minutes"]["max"]
+    assert TIME_UNIT["hours"]["min"] <= hours_difference <= TIME_UNIT["hours"]["max"]
+    assert TIME_UNIT["days"]["min"] <= days_difference <= TIME_UNIT["days"]["max"]
 
 
-def test_create_late_event_hours(late_events, setup, tollways):
-    late_event_ts = create_late_event(
-        events_log=late_events, fake=setup.get("faker"), tollways=tollways, time_unit="hours"
-    )
-
-    late_event_ts = datetime.strptime(late_event_ts["timestamp"], TIMESTAMP_FORMAT)
-    for ts in late_events["late_events"]["hours"]:
-        current_event_ts = datetime.strptime(ts, TIMESTAMP_FORMAT)
-        if late_event_ts.microsecond == current_event_ts.microsecond:
-            difference_in_minutes = abs(current_event_ts - late_event_ts).seconds // 3600
-            assert TIME_UNIT["hours"]["min"] <= difference_in_minutes <= TIME_UNIT["hours"]["max"]
-
-
-def test_create_late_event_days(late_events, setup, tollways):
-    late_event_ts = create_late_event(
-        events_log=late_events, fake=setup.get("faker"), tollways=tollways, time_unit="days"
-    )
-
-    late_event_ts = datetime.strptime(late_event_ts["timestamp"], TIMESTAMP_FORMAT)
-    for ts in late_events["late_events"]["days"]:
-        current_event_ts = datetime.strptime(ts, TIMESTAMP_FORMAT)
-        if late_event_ts.microsecond == current_event_ts.microsecond:
-            difference_in_days = abs(current_event_ts - late_event_ts).days
-            assert TIME_UNIT["days"]["min"] <= difference_in_days <= TIME_UNIT["days"]["max"]
-
-
-def test_create_duplicate_event(past_events):
-    duplicate_event = create_duplicate_event(events_log=past_events)
-    assert duplicate_event in past_events.get("past_events")
+def test_create_duplicate_event(duplicate_event, past_events):
+    assert duplicate_event in past_events["past_events"]
